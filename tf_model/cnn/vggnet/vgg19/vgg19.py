143a430f51c24f5b2089caf4a6990723e1b31c77
lovejing0306 <lovejing0306@gmail.com>
2018-06-10 22:08:30 +0800

    update vgg19

--- tf_model/cnn/vggnet/vgg19/vgg19.py
+++ tf_model/cnn/vggnet/vgg19/vgg19.py
@@ -1,9 +1,33 @@
 # coding=utf-8
 
 import tensorflow as tf
+import os
+import cifar10
 
-CLASS_NUM = 10
-DROPOUT_KEEP_PROB = 0.7
+image_size = 224
+channel_num = 3
+class_num = 10
+dropout_keep_prob = 0.7
+batch_size = 32
+
+num_examples_per_epoch_for_train = cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+
+dataset_dir = '../../dataset/cifar10'
+model_save_path = './model/'
+model_name = 'model.ckpt'
+
+def get_train_data(data_dir, batch_size):
+    cifar10.maybe_download_and_extract(data_dir)
+    images, labels = cifar10.get_distorted_train_batch(dataset_dir, batch_size)
+    images = tf.image.resize_images(images=images, size=[image_size, image_size])
+    return images, labels
+
+
+def get_eval_data(data_dir, batch_size):
+    cifar10.maybe_download_and_extract(data_dir)
+    images, labels = cifar10.get_undistorted_eval_batch(dataset_dir, batch_size)
+    images = tf.image.resize_images(images=images, size=[image_size, image_size])
+    return images, labels
 
 def weights_variable(shape, stddev=0.1, name='weights'):
     initial = tf.truncated_normal(shape=shape, stddev=stddev, dtype=tf.float32)
@@ -11,16 +35,8 @@ def weights_variable(shape, stddev=0.1, name='weights'):
 
 
 def biases_variable(shape, name='biases'):
-    initial = tf.constant(vaule=0.1, dtype=tf.float32, shape=shape)
+    initial = tf.constant(value=0.1, dtype=tf.float32, shape=shape)
     return tf.Variable(initial_value=initial, name=name)
-
-
-def conv2d(input, weigths, name='conv'):
-    return tf.nn.conv2d(input=input,
-                        filter=weigths,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME',
-                        name=name)
 
 
 def conv2d(input, filter_height, filter_width, num_channels, num_filters, name):
@@ -28,7 +44,11 @@ def conv2d(input, filter_height, filter_width, num_channels, num_filters, name):
         weigths = weights_variable(shape=[filter_height, filter_width, num_channels, num_filters],
                                    stddev=0.1)
         biases = biases_variable(shape=[num_filters])
-        conv = conv2d(input=input, weigths=weigths)
+        conv = tf.nn.conv2d(input=input,
+                            filter=weigths,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME',
+                            name=name)
         conv_out = tf.nn.relu(features=tf.nn.bias_add(value=conv, bias=biases), name='relu')
         return conv_out
 
@@ -113,38 +133,92 @@ def inference(inputs, train=True):
         pool5_flat = tf.reshape(tensor=pool5, shape=[-1, nodes], name='flat')
         fc6 = fc(input=pool5_flat, num_in=nodes, num_out=4096, name=scope)
         if train:
-            fc6_dropout = dropout(input=fc6, keep_prob=DROPOUT_KEEP_PROB, name=scope)
+            fc6_dropout = dropout(input=fc6, keep_prob=dropout_keep_prob, name=scope)
         else:
             fc6_dropout = dropout(input=fc6, keep_prob=1.0, name=scope)
 
     with tf.name_scope('fc7') as scope:
         fc7 = fc(input=fc6_dropout, num_in=4096, num_out=4096, name=scope)
         if train:
-            fc7_dropout = dropout(input=fc7, keep_prob=DROPOUT_KEEP_PROB, name=scope)
+            fc7_dropout = dropout(input=fc7, keep_prob=dropout_keep_prob, name=scope)
         else:
             fc7_dropout = dropout(input=fc7, keep_prob=1.0, name=scope)
 
     with tf.name_scope('fc8') as scope:
-        fc8 = fc(input=fc7_dropout, num_in=4096, num_out=CLASS_NUM, name=scope)
+        fc8 = fc(input=fc7_dropout, num_in=4096, num_out=class_num, name=scope)
 
     return fc8
 
 
 if __name__ == '__main__':
+    with tf.name_scope('Train_Batch'):
+        train_x, train_y = get_train_data(data_dir=dataset_dir, batch_size=batch_size)
+
+    with tf.name_scope('Valid_Batch'):
+        valid_x, valid_y = get_eval_data(data_dir=dataset_dir, batch_size=batch_size)
+
+    with tf.name_scope('Inputs'):
+        x = tf.placeholder(
+            dtype=tf.float32,
+            shape=[batch_size, image_size, image_size, channel_num],  # 第四维表示图片的深度，对于RBG格式的图片，深度为5
+            name='X')
+        y = tf.placeholder(
+            dtype=tf.int32,  # 不是one-hot时要为整型
+            shape=[batch_size],
+            name='Y')
+
     with tf.name_scope('Inference'):
-        y_hat = inference()
+        y_hat = inference(inputs=x)
 
     with tf.name_scope('Loss'):
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=None, logits=y_hat)
+        # 在没有one-hot时使用此种损失计算方法
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=y_hat)
         loss = tf.reduce_mean(cross_entropy)
 
     with tf.name_scope('Train'):
         global_step = tf.Variable(initial_value=0, trainable=False)
         optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
-        optimizer.minimize(loss=loss, global_step=global_step)
+        train = optimizer.minimize(loss=loss, global_step=global_step)
 
     with tf.name_scope('Evaluate'):
-        correct_pred = tf.equal(tf.argmax(y_hat, 1), tf.argmax(None, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        top_k = tf.nn.in_top_k(predictions=y_hat, targets=y, k=1, name='top_k')
+        # correct_pred = tf.equal(tf.argmax(y_hat, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(top_k, tf.float32))
 
     init = tf.global_variables_initializer()
+    # 初始化Tensorflow持久化类
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        print('===>>>>>>>==开始训练集上训练模型==<<<<<<<=====')
+        total_batches = int(num_examples_per_epoch_for_train / batch_size)
+        print('Per batch Size:,', batch_size)
+        print('Train sample Count Per Epoch:', num_examples_per_epoch_for_train)
+        print('Total batch Count Per Epoch:', total_batches)
+
+        # 启动数据读取队列
+        tf.train.start_queue_runners()
+        for epoch in range(10):
+            for i in range(total_batches):
+                batch_x, batch_y = sess.run(fetches=[train_x, train_y])
+                # print(batch_x.shape())
+                _, step = sess.run(fetches=[train, global_step],
+                                   feed_dict={x: batch_x, y: batch_y})
+                if step % 10 == 0:
+                    train_loss, train_eval = sess.run(fetches=[loss, accuracy],
+                                                      feed_dict={x: batch_x, y: batch_y})
+                    print(str(step) + '训练集损失：' + str(train_loss))
+                    print(str(step) + '训练集评估：' + str(train_eval))
+
+                    batch_x, batch_y = sess.run(fetches=[train_x, train_y])
+                    validation_loss, validation_eval = sess.run(fetches=[loss, accuracy],
+                                                                feed_dict={x: batch_x, y: batch_y})
+                    print(str(step) + '验证集损失：' + str(validation_loss))
+                    print(str(step) + '验证集评估：' + str(validation_eval))
+
+                    # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函数大小。
+                    # 在验证数据集上的正确率信息会有一个单独的程序来生成。
+                    # print("After %d training step(s), loss on training batch is %f." % (step, loss_value))
+                    # 保存当前的模型。注意这里隔出了global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮后得到的模型
+                    saver.save(sess, os.path.join(model_save_path, model_name), global_step=global_step)
